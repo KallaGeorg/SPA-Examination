dc10beaadfb9f4ad1df4bbc8ab074907897d0124
Georg <kalla.georg@yahoo.com>
2024-04-08 12:37:40 +0200

    Meny
    First Skizz

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import Meny from './components/Meny'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Meny />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Meny.tsx
@@ -0,0 +1,20 @@
+function Meny() {
+    return (
+        <div id='headerField'>
+        <div className='container'>
+  
+        <h1 id='head1'>Heaven SPA</h1>
+  
+       <div id='linksHeadMenue'>
+       <a >om oss</a>
+       <a >erbjudanden</a>
+       <a >boka</a>
+       <a >öppettider</a>
+       </div>
+    
+         </div>
+      </div>
+    );
+}
+
+export default Meny;
